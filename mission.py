2558228674696c661537f6c8a6630763eb5f56e3
Kocsi-HorvathMartin <kohomar@gmail.com>
2024-05-28 11:41:03 +0200

    Kész, kommentelve

--- mission.py
+++ mission.py
@@ -9,13 +9,13 @@ def akt_poz():           #Jelenlegi pozícióba
     msg=connection.recv_match(type='GLOBAL_POSITION_INT', blocking=True)
     return msg
 
-def mode(mode):
+def mode(mode):          #Mód váltás azonosító alapján
     connection.mav.set_mode_send(
                 connection.target_system,
                 mavutil.mavlink.MAV_MODE_FLAG_CUSTOM_MODE_ENABLED,
                 mode)
 
-def arm(arm):
+def arm(arm):            #Armolás/disarmolás
         connection.mav.command_long_send(connection.target_system,
                                  connection.target_component, 
                                  mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM, 
@@ -23,19 +23,17 @@ def arm(arm):
         msg=connection.recv_match(type='COMMAND_ACK', blocking=True)
         print(msg)
 
-def item(frame, command, current, autocontinue, param1, param2, param3, param4, param5, param6, param7):
+def item(frame, command, current, autocontinue, param1, param2, param3, param4, param5, param6, param7):    #Egy mission elem hozzáfűzése az adott missionhöz
      global mission
      mission.append([frame, command,current, autocontinue, param1, param2, param3,param4,param5,param6,param7])
 
-def feltolt():
+def feltolt():          #Mission feltöltése
     global mission
     n=len(mission)
-    print(n)
     connection.mav.mission_count_send(connection.target_system,
                                     connection.target_component,
                                     n)
     for i in range(n):
-        print(i)
         msg=connection.recv_match(type='MISSION_REQUEST', blocking=True)
         print(msg)
         connection.mav.mission_item_int_send(connection.target_system,
@@ -54,31 +52,27 @@ def feltolt():
                                     mission[i][10])
     msg=connection.recv_match(type='MISSION_ACK', blocking=True)
     print(msg)
-def start():
+
+def start():            #Mission elindítása
     connection.mav.command_long_send(connection.target_system, 
                                 connection.target_component,
                                 mavutil.mavlink.MAV_CMD_MISSION_START,
                                 0,0,0,0,0,0,0,0)
-    mode(4)
-    arm(1)
-    mode(3)
+    mode(4) #Guided mode
+    arm(1)  #Armolás
+    mode(3) #Auto mód
+
 #-----Main-----
 connection=mavutil.mavlink_connection('tcp:127.0.0.1:5762')
 connection.wait_heartbeat()
 print("Heartbeat from system (system %u component %u)" % (connection.target_system, connection.target_component))
 poz=akt_poz()
-print(poz)
 mission=[]
-item(mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,mavutil.mavlink.MAV_CMD_NAV_WAYPOINT,1,1,
-     0.0,2.0,1.0,math.nan,poz.lat,poz.lon,0)
 
-item(mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,mavutil.mavlink.MAV_CMD_NAV_TAKEOFF,0,1,
-     0,0,0,math.nan,poz.lat,poz.lon,2)
+item(mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,mavutil.mavlink.MAV_CMD_NAV_WAYPOINT,1,1,0.0,2.0,1.0,math.nan,poz.lat,poz.lon,0)         #Jelenlegi pozíció megadása
+item(mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,mavutil.mavlink.MAV_CMD_NAV_TAKEOFF,0,1,0,0,0,math.nan,poz.lat,poz.lon,2)                #Felszállás
+item(mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,mavutil.mavlink.MAV_CMD_NAV_WAYPOINT,0,1,0.0,2.0,1.0,math.nan,-353617755,1491651469,2.0) #Waypointra mozgás
+item(mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,mavutil.mavlink.MAV_CMD_NAV_LAND,0,0,0.0,0.0,0.0,math.nan,-353617755,1491651469,0.0)     #Leszállás
 
-item(mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,mavutil.mavlink.MAV_CMD_NAV_WAYPOINT,0,1,
-     0.0,2.0,1.0,math.nan,-353617755,1491651469,2.0)
-
-item(mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,mavutil.mavlink.MAV_CMD_NAV_LAND,0,0,
-     0.0,0.0,0.0,math.nan,-353617755,1491651469,0.0)
 feltolt()
 start()
