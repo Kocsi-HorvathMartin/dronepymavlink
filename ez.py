d024b63b4b72771259486a2cddfad86e46020620
Kocsi-Horváth Martin <kocsi.horvath.martin@hallgato.sze.hu>
2024-06-12 15:36:25 +0200

    ez.py működik, kikommentelve

--- ez.py
+++ ez.py
@@ -1,16 +1,17 @@
 from pymavlink import mavutil
 import math
+import os
 import time
-import matplotlib.pyplot as plt
 
-def get_line_params(x1, y1, x2, y2):
+def get_line_params(x1, y1, x2, y2):                    #Eyenes egyenlete 2 pontra
     if x1 == x2:  # vertical line
         return None, x1
     m = (y2 - y1) / (x2 - x1)
     b = y1 - m * x1
     return m, b
 
-def find_intersection(x1, y1, x2, y2, x3, y3, x4, y4):
+def find_intersection(x1, y1, x2, y2, x3, y3, x4, y4):  #Van-e két szakasznak metszéspontja
+    global inside
     m1, b1 = get_line_params(x1, y1, x2, y2)
     m2, b2 = get_line_params(x3, y3, x4, y4)
     
@@ -30,11 +31,10 @@ def find_intersection(x1, y1, x2, y2, x3, y3, x4, y4):
 
     if min(x1, x2) <= x <= max(x1, x2) and min(y1, y2) <= y <= max(y1, y2) and \
        min(x3, x4) <= x <= max(x3, x4) and min(y3, y4) <= y <= max(y3, y4):
-        return x, y
-    else:
-        return None
+        return True
+    return False
 
-def head_irany(fok, lat, lon):    #headingnek megfelelő irányba történő elmozdulás
+def head_irany(fok, lat, lon):              #headingnek megfelelő irányba történő elmozdulás
     fok = fok / 100
     if fok >= 0 and fok <= 90:
         lat += 2700 * math.cos(math.radians(fok))
@@ -50,7 +50,7 @@ def head_irany(fok, lat, lon):    #headingnek megfelelő irányba történő elm
         lon += 2700 * math.sin(math.radians(fok))
     return lat, lon
 
-def akt_poz(connection):           #Jelenlegi pozícióba
+def akt_poz(connection):                    #Jelenlegi pozícióba
     connection.mav.command_long_send(connection.target_system,
                                      connection.target_component, 
                                      mavutil.mavlink.MAV_CMD_REQUEST_MESSAGE, 
@@ -58,20 +58,10 @@ def akt_poz(connection):           #Jelenlegi pozícióba
     msg = connection.recv_match(type='GLOBAL_POSITION_INT', blocking=True)
     return msg
 
-def is_point_in_polygon(point, polygon):
-    """
-    Determine if the point is inside the polygon using the Ray Casting algorithm.
-
-    Parameters:
-    point: tuple, a point represented as (x, y).
-    polygon: list of tuples, the polygon represented as a list of (x, y) points.
-
-    Returns:
-    True if the point is inside the polygon, False otherwise.
-    """
+def is_point_in_polygon(point, polygon):    #Egy pont ellenőrzése hogy a poligonon belül taláható-e
+    inside=False
     x, y = point
     n = len(polygon)
-    inside = False
 
     p1x, p1y = polygon[0]
     for i in range(n + 1):
@@ -84,49 +74,37 @@ def is_point_in_polygon(point, polygon):
                     if p1x == p2x or x <= xinters:
                         inside = not inside
         p1x, p1y = p2x, p2y
-
     return inside
 
-def plot_polygon_and_path(polygon, dron_pos, path_end, intersections):
-    plt.figure()
-    if polygon:
-        plt.plot([p[0] for p in polygon] + [polygon[0][0]], [p[1] for p in polygon] + [polygon[0][1]], 'b-', label='Polygon')
-    plt.plot([dron_pos[0], path_end[0]], [dron_pos[1], path_end[1]], 'r-', label='Drone Path')
-    if intersections:
-        plt.plot([i[0] for i in intersections], [i[1] for i in intersections], 'go', label='Intersections')
-    plt.legend()
-    plt.xlabel('Longitude')
-    plt.ylabel('Latitude')
-    plt.title('Polygon and Drone Path')
-    plt.grid(True)
-    plt.show()
+def fence_download():                       #Fence letöltése a drónról 
+    global connection, x, y, polygon
+    connection.mav.mission_request_list_send(connection.target_system,
+                                         connection.target_component,
+                                         1)
+    msg = connection.recv_match(type='MISSION_COUNT', blocking=True)
+    
+    for i in range(msg.count):
+        connection.mav.mission_request_int_send(connection.target_system,
+                                                connection.target_component,
+                                                i, msg.mission_type)
+        elem = connection.recv_match(type='MISSION_ITEM_INT', blocking=True)
+        if elem is not None:
+            x.append(elem.x)
+            y.append(elem.y)
+            polygon.append((elem.x, elem.y))
+        else:
+            print(f"Failed to receive mission item {i}")
 
 connection = mavutil.mavlink_connection('tcp:127.0.0.1:5762')
 connection.wait_heartbeat()
 print("Heartbeat from system (system %u component %u)" % (connection.target_system, connection.target_component))
+
 poz = akt_poz(connection)
-
-connection.mav.mission_request_list_send(connection.target_system,
-                                         connection.target_component,
-                                         1)
-msg = connection.recv_match(type='MISSION_COUNT', blocking=True)
-
 polygon = []
 x = []
 y = []
 
-for i in range(msg.count):
-    connection.mav.mission_request_int_send(connection.target_system,
-                                             connection.target_component,
-                                             i, msg.mission_type)
-    elem = connection.recv_match(type='MISSION_ITEM_INT', blocking=True)
-    if elem is not None:
-        print(elem)
-        x.append(elem.x)
-        y.append(elem.y)
-        polygon.append((elem.x, elem.y))
-    else:
-        print(f"Failed to receive mission item {i}")
+fence_download()
 
 if polygon:
     polygon.append(polygon[0])
@@ -135,35 +113,18 @@ if polygon:
 else:
     print("Nincs kapott polygon pont")
 
-print(x)
-print(y)
 
-szakasz_pol = []
-for i in range(len(polygon) - 1):
-    szakasz_pol.append(get_line_params(x[i], y[i], x[i+1], y[i+1]))
-print(szakasz_pol)
-
-pont_hely = head_irany(poz.hdg, poz.lat, poz.lon)
-print(pont_hely)
-szakasz_hely = get_line_params(pont_hely[0], pont_hely[1], poz.lat, poz.lon)
-print(szakasz_hely)
-
-intersections = []
-for i in range(len(polygon) - 1):
-    intersection = find_intersection(pont_hely[0], pont_hely[1], poz.lat, poz.lon, x[i], y[i], x[i+1], y[i+1])
-    if intersection:
-        intersections.append(intersection)
-
-start_inside = is_point_in_polygon((poz.lat, poz.lon), polygon)
-end_inside = is_point_in_polygon(pont_hely, polygon)
-
-if intersection!=None or start_inside or end_inside:
-    print("True")
-else:
-    print("False")
-
-
-if polygon:
-    plot_polygon_and_path(polygon, (poz.lat, poz.lon), pont_hely, intersections)
-else:
-    print("Nincs polygonon.")
+while True:
+    vizsgalt=False
+    poz = akt_poz(connection)
+    pont_hely = head_irany(poz.hdg, poz.lat, poz.lon)
+    if is_point_in_polygon((poz.lat, poz.lon), polygon) or is_point_in_polygon(pont_hely, polygon):
+        vizsgalt=True
+    else:
+        for i in range(len(polygon) - 1):
+            if find_intersection(pont_hely[0], pont_hely[1], poz.lat, poz.lon, x[i], y[i], x[i+1], y[i+1]):
+                vizsgalt=True
+                break
+    print(vizsgalt)
+    time.sleep(0.1)
+    os.system('clear')
--- fence.py
+++ fence.py
@@ -2,6 +2,7 @@ from pymavlink import mavutil
 import math
 import matplotlib.pyplot as plt
 import time
+import os
 
 def is_point_in_polygon(point, polygon):
     """
@@ -55,17 +56,17 @@ def head_irany(fok):    #headingnek megfelelő irányba történő elmozdulás
     #egyseg=1/(111320*math.cos(math.radians(poz.lat*math.pow(10,-7))))
     fok=fok/100
     if fok>=0 and fok<=90:
-        lat+=27000*math.cos(math.radians(fok))
-        lon+=27000*math.sin(math.radians(fok))
+        lat+=2700*math.cos(math.radians(fok))
+        lon+=2700*math.sin(math.radians(fok))
     elif fok>90 and fok<=180:
-        lat+=27000*math.cos(math.radians(fok))
-        lon+=27000*math.sin(math.radians(fok))
+        lat+=2700*math.cos(math.radians(fok))
+        lon+=2700*math.sin(math.radians(fok))
     elif fok>180 and fok<=270:
-        lat+=27000*math.cos(math.radians(fok))
-        lon+=27000*math.sin(math.radians(fok))
+        lat+=2700*math.cos(math.radians(fok))
+        lon+=2700*math.sin(math.radians(fok))
     elif fok>270 and fok<360:
-        lat+=27000*math.cos(math.radians(fok))
-        lon+=27000*math.sin(math.radians(fok))
+        lat+=2700*math.cos(math.radians(fok))
+        lon+=2700*math.sin(math.radians(fok))
     return lat, lon
 
 def akt_poz():           #Jelenlegi pozícióba
@@ -93,73 +94,77 @@ def fence_szakaszon_van(i):
         if sorba[0]<=y_m and sorba[1]>=y_m:
             benne_van=True
 
+def ellenoriz():
+    global szakasz_pol, szakasz_hely, x, poz, benne_van, pont_hely
+
+    for i in range(len(szakasz_pol)):
+        if szakasz_pol[i][0]==None:
+            sorba=sorrend(poz.lat,pont_hely[0])
+            if sorba[0]<=x[i] and sorba[1]>=x[i]:
+                benne_van=True
+        elif szakasz_hely[0]==None:
+            sorba=sorrend(x[i],x[i+1])
+            if sorba[0]<=poz.lat and sorba[1]>=poz.lat:
+                benne_van=True
+        else:
+            x_m=(szakasz_pol[i][1]-szakasz_hely[1])/(szakasz_pol[i][0]-szakasz_hely[0])
+            y_m=szakasz_hely[0]*x_m+szakasz_hely[1]
+            sorba=sorrend(poz.lat, pont_hely[0])
+            plt.plot(x_m,y_m,'o')
+            if dron_szakaszon_van(x_m,y_m):
+                fence_szakaszon_van(i)
+
+def fence_download():
+    global connection, x, y, polygon
+    connection.mav.mission_request_list_send(connection.target_system,
+                                         connection.target_component,
+                                         1)
+    msg=connection.recv_match(type='MISSION_COUNT', blocking=True)
+
+    for i in range(msg.count):
+        connection.mav.mission_request_int_send(connection.target_system,
+                                                connection.target_component,
+                                                i,msg.mission_type)
+        elem=connection.recv_match(type='MISSION_ITEM_INT', blocking=True)
+        x.append(elem.x)
+        y.append(elem.y)
+        polygon.append((elem.x,elem.y))
+
+def fence_szakaszok():
+    global x, y, szakasz_pol
+    x.append(x[0])
+    y.append(y[0])
+    for i in range(0,len(x)-1):
+        szakasz_pol.append(egyenlet(x[i],y[i],x[i+1],y[i+1]))
+
+def dron_szakasz():
+    global pont_hely, szakasz_hely
+    pont_hely=head_irany(poz.hdg)
+    szakasz_hely=egyenlet(pont_hely[0],pont_hely[1],poz.lat,poz.lon)
+
 connection=mavutil.mavlink_connection('tcp:127.0.0.1:5762')
 connection.wait_heartbeat()
 print("Heartbeat from system (system %u component %u)" % (connection.target_system, connection.target_component))
-poz=akt_poz()
 
-connection.mav.mission_request_list_send(connection.target_system,
-                                         connection.target_component,
-                                         1)
-msg=connection.recv_match(type='MISSION_COUNT', blocking=True)
-#print(msg)
 polygon=[]
 x=[]
 y=[]
-for i in range(msg.count):
-    connection.mav.mission_request_int_send(connection.target_system,
-                                             connection.target_component,
-                                             i,msg.mission_type)
-    elem=connection.recv_match(type='MISSION_ITEM_INT', blocking=True)
-    #print(elem)
-    x.append(elem.x)
-    y.append(elem.y)
-    polygon.append((elem.x,elem.y))
+fence_download()
 
-x.append(x[0])
-y.append(y[0])
-#print(x)
-#print(y)
+
 szakasz_pol=[]
-for i in range(0,msg.count):
-    szakasz_pol.append(egyenlet(x[i],y[i],x[i+1],y[i+1]))
-#print(szakasz_pol)
-
-pont_hely=head_irany(poz.hdg)
-print(pont_hely)
-szakasz_hely=egyenlet(pont_hely[0],pont_hely[1],poz.lat,poz.lon)
-#print(szakasz_hely)
+szakasz_hely=[]
+pont_hely=[]
+poz=akt_poz()
+fence_szakaszok()
+dron_szakasz()
 benne_van=False
 if is_point_in_polygon((poz.lat,poz.lon),polygon):
     benne_van=True
-    
-for i in range(len(szakasz_pol)):
-    if szakasz_pol[i][0]==None:
-        sorba=sorrend(poz.lat,pont_hely[0])
-        if sorba[0]<=x[i] and sorba[1]>=x[i]:
-            benne_van=True
-    elif szakasz_hely[0]==None:
-        sorba=sorrend(x[i],x[i+1])
-        if sorba[0]<=poz.lat and sorba[1]>=poz.lat:
-            benne_van=True
-    else:
-        x_m=(szakasz_pol[i][1]-szakasz_hely[1])/(szakasz_pol[i][0]-szakasz_hely[0])
-        y_m=szakasz_hely[0]*x_m+szakasz_hely[1]
-        plt.plot(x_m,y_m)
-        sorba=sorrend(poz.lat, pont_hely[0])
-        if dron_szakaszon_van(x_m,y_m):
-            fence_szakaszon_van(i)
+ellenoriz()
 print(benne_van)
 
-for i in range(len(x)-1):
-    x_array=[x[i],x[i+1]]
-    y_array=[y[i],y[i+1]]
-    plt.plot(x_array,y_array)
-
-
-x_array=[poz.lat,pont_hely[0]]
-y_array=[poz.lon,pont_hely[1]]
-plt.plot(x_array,y_array)
-plt.plot(poz.lat,poz.lon,"o")
-plt.plot(pont_hely[0],pont_hely[1],"o")
+plt.plot(x,y)
+#plt.plot(poz.lat,poz.lon,'or')
+#plt.plot(pont_hely[0],pont_hely[1],'og')
 plt.show()
