f11de5a765f7eb7ce5bfcd2b0c1cc3f6c6b236eb
Kocsi-Horváth Martin <kocsi.horvath.martin@hallgato.sze.hu>
2024-06-18 16:00:33 +0200

    Koordináták megfelelő formátumba, nem jó

--- fence_upload.py
+++ fence_upload.py
@@ -11,15 +11,20 @@ def beolvas(file_path):
     # Now `data` contains the parsed JSON content
     features=data['features']
     for i in range(len(features)):
+        print(i)
         coordinates=features[i]['geometry'][0]['horizontalProjection']['coordinates']
         for j in range(len(coordinates[0])):
             coordinate=coordinates[0][j]
+            lat=round(coordinate[0],7)*(10**7)
+            lon=round(coordinate[1],7)*(10**7)
+            #print(int(lat),int(lon))
             item(mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,
                  mavutil.mavlink.MAV_CMD_NAV_FENCE_POLYGON_VERTEX_EXCLUSION,
                  0,0,
-                 len(coordinates[0]),0,0,0,coordinate[0],coordinate[1],0)
+                 len(coordinates[0]),0,0,0,int(lat),int(lon),0)
         feltolt()
         mission=[]
+        print()
 
 def akt_poz():           #Jelenlegi pozícióba
     connection.mav.command_long_send(connection.target_system,
@@ -41,8 +46,8 @@ def feltolt():          #Mission feltöltése
                                     n,1,0)
     for i in range(n):
         msg=connection.recv_match(type='MISSION_REQUEST', blocking=True)
-        print(msg)
-        connection.mav.mission_item_send(connection.target_system,
+        #print(msg)
+        connection.mav.mission_item_int_send(connection.target_system,
                                     connection.target_component,
                                     i,
                                     mission[i][0],
@@ -66,5 +71,12 @@ connection.wait_heartbeat()
 print("Heartbeat from system (system %u component %u)" % (connection.target_system, connection.target_component))
 poz=akt_poz()
 print(poz)
+connection.mav.mission_clear_all_send(connection.target_system, connection.target_component,1)
+msg=connection.recv_match(type='MISSION_ACK',blocking=True)
+print(msg)
 mission=[]
 beolvas('/home/kocsi-horvath/Documents/uav_202406181054.json')
+connection.mav.command_long_send(connection.target_system,
+                                 connection.target_component,
+                                 mavutil.mavlink.MAV_CMD_DO_FENCE_ENABLE,
+                                 0,1,int(0b00010),0,0,0,0,0)
